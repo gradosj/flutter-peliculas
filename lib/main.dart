5ee6152bd39b15533698a00c3773470ab0038509
JoSe <gradosj@gmail.com>
2022-12-06 09:13:04 +0100

    Clase 111

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/screens/screens.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,15 +9,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Peliculas',
+      initialRoute: 'home',
+      routes: {
+        'home': (_) => HomeScreen(),
+        'details': (_) => DetailsScreen(),
+      },
+      theme: ThemeData.light()
+          .copyWith(appBarTheme: AppBarTheme(color: Colors.indigo)),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/widgets/widgets.dart';
 
-class DetailsScreen extends StatelessWidget {
-  const DetailsScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Peliculas de cine'),
+        elevation: 0,
+        //     iconTheme: IconTheme.of(context),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.search_outlined))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // tarjetas principales
+            CardSwiper(),
+            //slider de peliculas
+            MovieSlider(),
+          ],
+        ),
+      ),
+    );
   }
 }
-gitº
